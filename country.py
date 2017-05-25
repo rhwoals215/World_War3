61d7a803e9a4ec85d819515fe0bb3a3364f92155
Jaemin Ko <rhwoals215@gmail.com>
2017-05-25 17:43:55 +0900

    made class variables, made continent, country menu

---
+++ country.py
@@ -0,0 +1,14 @@
+class Country:
+    def __init__(self, name, capital, population, money, wood, metal, water, army_level, defense_level, research_level, trade_level, tax_rate, citizen_happiness, alliance, turn):
+        self.name = name
+        self.capital = capital
+        self.population = population
+        self.money = money
+        self.army_level = army_level
+        self.defense_level = defense_level
+        self.research_level = research_level
+        self.trade_level = trade_level
+        self.tax_rate = tax_rate
+        self.citizen_happiness = citizen_happiness
+        self.alliance = alliance
+        self.turn = turn
---
+++ worldwar3.py
@@ -0,0 +1,56 @@
+os.system("clear")
+print("                     World War 3")
+print("                     made by Jaemin Ko")
+print("")
+press_enter = raw_input("Press enter.")
+country = Country("", "", 1000, 1000000, 1, 1, 1, 1, 0, 100, 0):
+os.system("clear")
+print("Continents")
+print("1. North America")
+print("2. South America")
+print("3. Europe")
+print("4. Asia")
+print("5. Africa")
+print("6. Austrailia")
+print("7. Antartica")
+continent_choice = raw_input("Choose Your Continent. ")
+if continent_choice = 1:
+    print("North America:")
+    print("1. United States")
+    country_choice = raw_input("Choose Your Country. ")
+
+elif continent_choice = 2:
+    print("South America:")
+    print("1. Brazil")
+    print("2. Mexico")
+    country_choice = raw_input("Choose Your Country. ")
+
+elif continent_choice = 3:
+    print("1. United Kingdom"
+    print("2. Germany")
+    print("3. France")
+    print("4. Spain")
+    print("5. Italy")
+    print("6. Russia")
+    print("7. Switzerland")
+    country_choice = raw_input("Choose Your Country. ")
+
+elif continent_choice = 4:
+    print("1. China")
+    print("2. India")
+    print("3. South Korea")
+    print("4. Japan")
+    print("5. Indonesia")
+    print("6. Turkey")
+    print("7. ")
+    country_choice = raw_input("Choose Your Country. ")
+
+elif continent_choice = 5:
+    print("1. Nigeria")
+    print("2. South Africa")
+    country_choice = raw_input("Choose Your Country. ")
+
+while turn:
+    print("Notification:")
+    notification()
+    print("choices:")
